8ede3fc7874ccaeafb6f29be6d1d2aa3c45ca8eb
Ujjwal Kirti <167794456+ujjwalkirtibpa@users.noreply.github.com>
2025-12-29 18:14:13 +0530

    add: policy page

---
+++ app/policies/page.tsx
@@ -0,0 +1,21 @@
+function PolicyPage() {
+	return (
+		<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
+			<h1 className="text-5xl font-bold font-[amaranth] mb-8">Policies</h1>
+
+			<p className=" leading-relaxed mb-8">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisl ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+
+			<ul className="list-disc list-inside space-y-2 ">
+				<li>Lorem Ipsum</li>
+				<li>Lorem Ipsum</li>
+				<li>Lorem Ipsum</li>
+				<li>Lorem Ipsum</li>
+			</ul>
+		</div>
+	);
+}
+
+export default PolicyPage;
